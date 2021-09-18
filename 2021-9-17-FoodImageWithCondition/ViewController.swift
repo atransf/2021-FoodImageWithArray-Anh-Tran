89cbc2154d605b3125815d71666f51bf43fdbdb9
Anh Tran <lamanh309@gmail.com>
2021-09-17 22:00:33 -0700

    Food Image with If and Else

--- 2021-9-17-FoodImageWithCondition/ViewController.swift
+++ 2021-9-17-FoodImageWithCondition/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var foodImageView: UIImageView!
+    @IBOutlet weak var foodPriceLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        foodImageView.image = UIImage(named: "pizza")
+        foodPriceLable.text = "🍕 Pizza: $19.99"
+    }
+    
+    @IBAction func nextDishButtonPressed(_ sender: UIButton) {
+        
+        if foodPriceLable.text!.contains("Pizza") {
+            foodImageView.image = UIImage(named: "hamburger")
+            foodPriceLable.text = ("🍔 Burger: $15.99")
+        } else if foodPriceLable.text!.contains("Burger") {
+            foodImageView.image = UIImage(named: "salad")
+            foodPriceLable.text = ("🥗 Salad: $12.99")
+        }  else if foodPriceLable.text!.contains("Salad") {
+            foodImageView.image = UIImage(named: "pasta")
+            foodPriceLable.text = ("🍝 Pasta: $21.99")
+        } else {
+            foodImageView.image = UIImage(named: "pizza")
+            foodPriceLable.text = "🍕 Pizza: $19.99"
+        }
+        
     }
 
 
 }
-
