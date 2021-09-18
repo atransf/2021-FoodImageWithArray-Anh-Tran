83312240743c7809941e9293a222407fb60ce259
Anh Tran <lamanh309@gmail.com>
2021-09-17 20:47:11 -0700

    Initial Commit

---
+++ 2021-9-17-FoodImageWithCondition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2021-9-17-FoodImageWithCondition
+//
+//  Created by Anh Tran on 9/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 2021-9-17-FoodImageWithConditionTests/_021_9_17_FoodImageWithConditionTests.swift
@@ -0,0 +1,33 @@
+//
+//  _021_9_17_FoodImageWithConditionTests.swift
+//  2021-9-17-FoodImageWithConditionTests
+//
+//  Created by Anh Tran on 9/17/21.
+//
+
+import XCTest
+@testable import _021_9_17_FoodImageWithCondition
+
+class _021_9_17_FoodImageWithConditionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
